e6cf24ee1fdb5c569731fd7492a5b664e2f63f64
Eric Andrews <eric.andrews@streamr.com>
2017-02-13 16:13:33 +0200

    dev.streamr pm2 configuraiton

--- dev-streamr.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-java -jar build/libs/broker-*-all.jar -Dkafka.server="dev.streamr:9093" -Dkafka.group="data-dev" -Dkafka.topic="data-dev" -Dredis.host="dev.streamr" -Dredis.password="AFuPxeVMwBKHV5Hm5SK3PkRZA" -Dcassandra.host="dev.streamr" -Dcassandra.keyspace="streamr_dev" -Dstats.interval="60"
