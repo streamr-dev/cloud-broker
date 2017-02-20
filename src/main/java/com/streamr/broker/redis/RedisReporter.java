c5c884bbeebd295913c16901c08c14c9f3c96fe4
Eric Andrews <eric.andrews@streamr.com>
2017-02-20 19:58:00 +0200

    RedisReporter: put offsets to key-value store

--- src/main/java/com/streamr/broker/redis/RedisReporter.java
+++ src/main/java/com/streamr/broker/redis/RedisReporter.java
@@ -2,28 +2,31 @@ package com.streamr.broker.redis;
 
 import com.lambdaworks.redis.RedisClient;
 import com.lambdaworks.redis.RedisURI;
+import com.lambdaworks.redis.api.async.RedisAsyncCommands;
 import com.lambdaworks.redis.codec.ByteArrayCodec;
 import com.lambdaworks.redis.pubsub.api.async.RedisPubSubAsyncCommands;
-import com.streamr.broker.StreamrBinaryMessageWithKafkaMetadata;
 import com.streamr.broker.Reporter;
+import com.streamr.broker.StreamrBinaryMessageWithKafkaMetadata;
 import com.streamr.broker.stats.Stats;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.nio.charset.StandardCharsets;
-import java.util.concurrent.Semaphore;
 
 public class RedisReporter implements Reporter {
 	private static final Logger log = LogManager.getLogger();
+	private static final int OFFSET_EXPIRATION_IN_SEC = 5;
 
 	private final RedisClient client;
-	private final RedisPubSubAsyncCommands<byte[], byte[]> connection;
+	private final RedisPubSubAsyncCommands<byte[], byte[]> pubSub;
+	private final RedisAsyncCommands<String, String> commands;
 	private Stats stats;
 
 	public RedisReporter(String host, String password) {
 		RedisURI uri = RedisURI.Builder.redis(host).withPassword(password).build();
 		client = RedisClient.create(uri);
-		connection = client.connectPubSub(new ByteArrayCodec()).async();
+		pubSub = client.connectPubSub(new ByteArrayCodec()).async();
+		commands = client.connect().async();
 		log.info("Redis connection created for " + uri);
 	}
 
@@ -34,18 +37,20 @@ public class RedisReporter implements Reporter {
 
 	@Override
 	public void report(StreamrBinaryMessageWithKafkaMetadata msg) {
-		connection.publish(formKey(msg), msg.toBytesWithKafkaMetadata())
+		String key = formKey(msg);
+		pubSub.publish(key.getBytes(StandardCharsets.UTF_8), msg.toBytesWithKafkaMetadata())
 			.thenRunAsync(() -> stats.onWrittenToRedis(msg));
+		commands.setex(key, OFFSET_EXPIRATION_IN_SEC, String.valueOf(msg.getOffset()));
 	}
 
 	@Override
 	public void close() {
-		connection.close();
+		pubSub.close();
+		commands.close();
 		client.shutdown();
 	}
 
-	private static byte[] formKey(StreamrBinaryMessageWithKafkaMetadata msg) {
-		String s = msg.getStreamId() + "-" + msg.getPartition();
-		return s.getBytes(StandardCharsets.UTF_8);
+	private static String formKey(StreamrBinaryMessageWithKafkaMetadata msg) {
+		return msg.getStreamId() + "-" + msg.getPartition();
 	}
 }
